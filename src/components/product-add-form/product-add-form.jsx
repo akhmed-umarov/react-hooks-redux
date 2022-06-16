816e22f099b88130caa90c75894e7c210aeda486
Akhmed <ahmad.umarov7@gmail.com>
2022-06-17 00:07:49 +0300

    3 day end

--- src/components/product-add-form/product-add-form.jsx
+++ src/components/product-add-form/product-add-form.jsx
@@ -1,16 +1,50 @@
+import { Component } from 'react';
 import './product-add-form.css';
 
 
-const ProductAddForm =()=>{ 
+
+class ProductAddForm extends Component { 
+constructor (props) { 
+    super(props);
+    this.state = { 
+        name: '' , 
+        salary: ""
+    }
+}
+onValueChange =(e)=>{ 
+    // console.log(e.target.name);      интересная конструкция
+    this.setState({
+        //то есть мы тут заменим e.target.name на name в случае если e.target.name = name и на salary если равен salary 
+        //то есть мы можем использовать такую запись если мы хотим поменять какие то атрибуты элементов но мы точно не знаем какой именно у нас будет там атрибут
+        [e.target.name]: e.target.value
+    })
+}
+
+
+
+
+render() { 
+
+  
+
+
+   let {name , salary} = this.state ;
+
    return (
       <div className="app-add-form">
       <h3>Добавьте нового сотрудника</h3>
       <form
-          className="add-form d-flex">
+      className="add-form d-flex">
           <input type="text"
+              name='name'
+              value={name}
+              onChange={this.onValueChange}
               className="form-control new-post-label"
               placeholder="Как его зовут?" />
           <input type="number"
+              name='salary'
+              value={salary}
+              onChange={this.onValueChange}
               className="form-control new-post-label"
               placeholder="З/П в $?" />
 
@@ -19,6 +53,6 @@ const ProductAddForm =()=>{
       </form>
   </div>
    )
-}
+}} 
 
 export default ProductAddForm
--- src/components/product-list-item/product-list-item.jsx
+++ src/components/product-list-item/product-list-item.jsx
@@ -1,36 +1,129 @@
+import { Component } from 'react';
 import './product-list-item.css';
 
-
-const ProductListItem = ({name , salary , increase}) =>{ 
-
-   const increaseFun = ()=>{ 
-      
-      if (increase){ 
-      return "list-group-item d-flex justify-content-between  increase"
-      } else {
-         return "list-group-item d-flex justify-content-between "
+class ProductListItem extends Component { 
+   constructor (props){ 
+      super(props); 
+      // лучше тут не добавлять все в state так как тогда все время  сюда буду гонять лишние данные 
+      this.state = {
+         // name: this.props.name, 
+         // salary: this.props.salary, 
+         increase: this.props.increase,
+         likeStar: false
       }
+      ///нет разницы где писать методы внутри конструктора или вне его , если методы указываются не через конструктор тогда нет необходимости указывать this
+      this.onIncrease = () =>{ 
+         this.setState(({increase})=>({
+           increase : !increase
+         }))
+        }
    }
 
-   return ( 
-      <li className={increaseFun()}>
-         <span className="list-group-item-label">{name}</span>
-         <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
-         <div className='d-flex justify-content-center align-items-center'>
-              <button type="button"
-                    className="btn-cookie btn-sm ">
-                    <i className="fas fa-cookie"></i>
-              </button>
+   // onIncrease = () =>{ 
+   //  this.setState(({increase})=>({
+   //    increase : !increase
+   //  }))
+   // }
+   
+   onStar = () =>{ 
+      this.setState(({likeStar})=> ({ 
+         likeStar: !likeStar
+      }))
+   }
 
-               <button type="button"
-                       className="btn-trash btn-sm ">
-                   <i className="fas fa-trash"></i>
-              </button>
-              <i className="fas fa-star"></i>
-           </div>
-       </li>
-   )
+ 
+   render() { 
+
+   const {name , salary } = this.props;
+   const {increase , likeStar} = this.state;
+
+   
+   // let increaseFun = ()=>{ 
+   // if (increase){ 
+   // return "list-group-item d-flex justify-content-between  increase"
+   // } else {
+   // return "list-group-item d-flex justify-content-between " 
+   // }
+   // }
+   
+   let classLi = "list-group-item d-flex justify-content-between ";
+   if (increase) {
+      classLi += " increase"
+   }
+   if (likeStar) { 
+      classLi += " like"
+   }
+
+      return ( 
+         <li className={classLi}>
+            <span
+            onClick={this.onStar}
+             className="list-group-item-label">{name}</span>
+            <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+            <div className='d-flex justify-content-center align-items-center'>
+                 <button type="button"
+                        onClick={this.onIncrease}
+                       className="btn-cookie btn-sm ">
+                       <i className="fas fa-cookie"></i>
+                 </button>
+   
+                  <button type="button"
+                          className="btn-trash btn-sm ">
+                      <i className="fas fa-trash"></i>
+                 </button>
+                 <i className="fas fa-star"></i>
+              </div>
+          </li>
+      )
+
+
+   }
+
+
+
+
+
+
 }
 
 
+
 export default ProductListItem
+
+
+
+
+
+
+// const ProductListItem = ({name , salary , increase}) =>{ 
+
+   
+
+//    const increaseFun = ()=>{ 
+      
+//       if (increase){ 
+//       return "list-group-item d-flex justify-content-between  increase"
+//       } else {
+//          return "list-group-item d-flex justify-content-between "
+//       }
+//    }
+
+//    return ( 
+//       <li className={increaseFun()}>
+//          <span className="list-group-item-label">{name}</span>
+//          <input type="text" className="list-group-item-input" defaultValue={salary + "$"}/>
+//          <div className='d-flex justify-content-center align-items-center'>
+//               <button type="button"
+//                     className="btn-cookie btn-sm ">
+//                     <i className="fas fa-cookie"></i>
+//               </button>
+
+//                <button type="button"
+//                        className="btn-trash btn-sm ">
+//                    <i className="fas fa-trash"></i>
+//               </button>
+//               <i className="fas fa-star"></i>
+//            </div>
+//        </li>
+//    )
+// }
